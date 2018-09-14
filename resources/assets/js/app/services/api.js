9e3ad64b8deeb44de675a2a22b8f4c2faa26c6fa
LauroRigon <laurorigon@gmail.com>
2018-09-14 17:08:26 -0300

    One more

--- resources/assets/js/app/services/api.js
+++ resources/assets/js/app/services/api.js
@@ -1,56 +1,43 @@
-import axios from 'axios'
-import store from '../store/configureStore'
-import { saveState } from 'app/localStorage'
-import { userLoggedOut, setToken } from '../actions/user'
+import axios from 'axios';
+import store from '../store/configureStore';
 
-const token = getCurrentToken()
+import { userLoggedOut } from '../actions/user';
+
+const token = getCurrentToken();
 
 const api = axios.create({
-  baseURL: 'https://floating-garden-78421.herokuapp.com/api/',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  }
-})
+    baseURL: '/api/',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+});
 
 /**
  * coloca o token antes de cada request
  */
 api.interceptors.request.use(config => {
-  config.headers['Authorization'] = `Bearer ${getCurrentToken()}`
-  return config
+  config.headers['Authorization'] = `Bearer ${getCurrentToken()}`;
+  console.log(config)
+  return config;
 })
 
 /**
- * se for erro 401, desloga o user,
- * se for response de sucesso, procura por um token refreshado no header
+ * se for erro 401, desloga o user
  */
-api.interceptors.response.use(
-  response => {
-    if(response.headers['authorization']) {
-      setNewToken(response.headers['authorization'])
-    }
-
-    return Promise.resolve(response)
-  },
-  error => {
-    if (error.response.status == 401) {
-      store.dispatch(userLoggedOut())
-    }
-
-    return Promise.reject(error)
+api.interceptors.response.use(response => response, error => {
+  if (error.response.status == 401) {
+    store.dispatch(userLoggedOut());
   }
-)
+
+  return Promise.reject(error);
+})
 
 /**
  * pega o token atual da store
  */
-function getCurrentToken () {
-  return store.getState().user.authToken
+function getCurrentToken() {
+  return store.getState().user.authToken;
 }
 
-function setNewToken(token) {
-  store.dispatch(setToken(token))
-  saveState(store)
-}
-export default api
+export default api;
