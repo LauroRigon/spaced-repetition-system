ab71f6c7017703852735d020f431326d07f2302a
LauroRigon <laurorigon@gmail.com>
2018-09-14 16:51:31 -0300

    Heroku send and allow origin

---
+++ app/Http/Middleware/SetGlobalHeaders.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SetGlobalHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        
+        return $response;
+    }
+}
