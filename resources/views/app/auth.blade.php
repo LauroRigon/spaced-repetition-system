c82544ed8655e5b69bd360306264765c14bb9e60
LauroRigon <laurorigon@gmail.com>
2019-02-18 16:16:32 -0300

    Fix

--- resources/views/app/auth.blade.php
+++ resources/views/app/auth.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <div id="root"></div>
-<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<!-- <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script> -->
 <script src="{{ mix('js/app.js') }}"></script>
 
 
